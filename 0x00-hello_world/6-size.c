abc75839d94d49dc8cac1e6f8a68d58347c8bd0a
yonipac <suraflmekonnen254@gmail.com>
2022-03-14 12:51:47 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,23 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: using sizeof to print the size of various types.
- *
- * Return: Always 0 (Succesful)
-*/
-
-int main(void)
-{	
-
-	/* sizeof evaluates the size of a variable */
-	int i;
-	double d;
-	char c;
-
-	prnitf("size of a char: %d byte(s)\n",sizeof(char));
-	printf("size of an int: %d byte(s)\n",sizeof (int));
-	printf("size of a long int: %d byte(s)\n",sizeof (long int));
-	printf("size of a long long int: %d byte(s)\n",sizeof(long long int));
-	printf("size of a float: %d byte(s)\n",sizeof (float));
-
-	return(0);
+#include <stdio.h>                                                                           
+                                                                                             
+/**                                                                                          
+ * main - Entry point                                                                        
+ *                                                                                           
+ * Description: using sizeof to print the size of various types.                             
+ *                                                                                           
+ * Return: Always 0 (Succesful)                                                              
+*/                                                                                           
+                                                                                             
+int main(void)                                                                               
+{                                                                                            
+	                                                                                             
+	        /* sizeof evaluates the size of a variable */                                        
+	                                                                                             
+	        printf("size of a char: %ld byte(s)\n", sizeof (char));                                 
+                printf("size of an int: %ld byte(s)\n", sizeof (int));                                 
+	        printf("size of a long int: %ld byte(s)\n", sizeof (long int));                        
+	        printf("size of a long long int: %ld byte(s)\n", sizeof (long long int));               
+	        printf("size of a float: %ld byte(s)\n", sizeof (float));                              
+						                                                                                             
+                return(0); 
 }
