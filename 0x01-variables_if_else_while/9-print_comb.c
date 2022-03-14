0c758b1a953455d187036397e244dca8cbaffbc9
yonipac <suraflmekonnen254@gmail.com>
2022-03-14 14:09:20 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,21 +10,18 @@
 
 int main(void)
 {
-	  int digit = 0;
+	int digit = 0;
 
-	  while (digit <= 9)
-	  {
-	  putchar(digit + 48);
-
-	  if (digit != 9)
-												       {
-													       putchar(',');
-													       putchar(' ');
-												       }
-
-												       ++digit;
- 												}
-   putchar('\n');
-
-   return (0);
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+		if (digit !=9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+		++digit;
+	}
+	putchar('\n');
+	return (0);
 }
