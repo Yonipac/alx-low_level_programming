6aa3b2ea1b5bfc4321d6597450881e9e0211c9b1
yonipac <suraflmekonnen254@gmail.com>
2022-03-14 14:03:08 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,14 +14,12 @@ int main(void)
 
 	while (digit <= 102) /*102; decimal rep of f*/
 	{
-				 
-		  putchar(digit);								
-		  /* after 9 we jump till 96; `*/
-	      if (digit == 57)
-														digit += 39;
-												            ++digit;
-												     }
-        putchar('\n');
-
+		putchar(digit);
+		/*after 9 we jump till 96; */
+		if (digit == 57)
+			digit += 39;
+		++digit;
+	}
+	putchar('\n');
 	return (0);
 }
