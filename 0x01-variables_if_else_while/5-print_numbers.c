ad5e6af2ff9add4392f246f2a012eba0d8aa8cbd
yonipac <suraflmekonnen254@gmail.com>
2022-03-14 13:50:17 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,22 +2,21 @@
 
 /**
  * main - Entry point
- *   
+ *
  * Description: print all single digit numbers
- * 
- * Return: Always 0 (Success)
-*/
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
-        int digit = 0;
+	int digit = 0;
 
 	while (digit <= 9)
 	{
-	printf("%i", digit);
-	++digit;
+		printf("%i", digit);
+		++digit;
 	}
 	printf("\n");
-
 	return (0);
 }
