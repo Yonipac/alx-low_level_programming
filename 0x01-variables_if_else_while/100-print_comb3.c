c0a798185cee61a2a4821701c48033516d9f4a5e
yonipac <suraflmekonnen254@gmail.com>
2022-03-14 14:22:25 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,23 +17,20 @@ int main(void)
 	{
 	digit2 = 0;
 	while (digit2 <= 9)
-												     {
-													if (digit1 != digit2 && digit1 < digit2)
-												      {
-																									putchar(digit1 + 48);
-																									putchar(digit2 + 48);
-
-																									if (digit1 + digit2 != 17)
-																																				{
-																																			   putchar(',');
-																																															putchar(' ');
-																																															}
-																																			   }
-												  ++digit2;
-												 }
-												  ++digit1;
-    }
-	putchar('\n');
-
+	{
+		if (digit1 != digit2 && digit1 < digit2)
+		{
+			putchar(digit1 + 48);
+			putchar(digit2 + 48);
+			if (digit1 + digit2 != 17)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		++digit2;
+	}
+	++digit1;
+	}putchar('\n');
 	return (0);
 }
