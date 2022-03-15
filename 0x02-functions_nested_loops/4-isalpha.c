8da3819c6b6a48643797886c60ce7396636788a2
Yonipac <surafelmekonnen254@gmail.com>
2022-03-15 12:48:11 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,13 +5,13 @@
  *           both lowercase or uppercase
  * @c: takes input from other function.
  *
- * Return: 1 is c if true else 0
+ * Return: 1 is 'c' if true else 0
  */
 
 int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
+			c >=65 && c >= 90)
 		return (1);
 	return (0);
 }
