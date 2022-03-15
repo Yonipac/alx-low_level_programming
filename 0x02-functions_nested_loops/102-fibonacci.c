9c27ddda546cd4bff2c08d1a34e3575349f78f2f
Yonipac <surafelmekonnen254@gmail.com>
2022-03-15 17:02:39 +0300

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,7 @@
 int main(void)
 {
 	int count;
-	unsigned long fib1 = 0, fib2 =1, sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
 	for (count = 0; count < 50; ++count)
 	{
@@ -25,7 +25,7 @@ int main(void)
 		if (count == 49)
 			printf("\n");
 		else
-			printf(",");
+			printf(", ");
 	}
 
 	return (0);
