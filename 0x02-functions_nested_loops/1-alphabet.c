442862370a9e482b63ca3aca806e672ae83ac246
Yonipac <surafelmekonnen254@gmail.com>
2022-03-15 12:19:21 +0300

    alphabetgame

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
  *                  the alphbet a - z
  */
 
-void print_alphabet(void)
+int print_alphabet(void)
 {
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ++ch)
 		_putchar(ch);
 	_putchar('\n');
-	return (0);
+	
 }
