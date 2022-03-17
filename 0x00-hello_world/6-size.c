f5fbea3a6e44a6186b787dbd67b8b71411a11862
Yonipac <surafelmekonnen254@gmail.com>
2022-03-17 14:45:53 +0300

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,23 +1,24 @@
-#include <stdio.h>                                                                           
-                                                                                             
-/**                                                                                          
- * main - Entry point                                                                        
- *                                                                                           
- * Description: using sizeof to print the size of various types.                             
- *                                                                                           
- * Return: Always 0 (Succesful)                                                              
-*/                                                                                           
-                                                                                             
-int main(void)                                                                               
-{                                                                                            
-	                                                                                             
-	        /* sizeof evaluates the size of a variable */                                        
-	                                                                                             
-	        printf("size of a char: %ld byte(s)\n", sizeof (char));                                 
-                printf("size of an int: %ld byte(s)\n", sizeof (int));                                 
-	        printf("size of a long int: %ld byte(s)\n", sizeof (long int));                        
-	        printf("size of a long long int: %ld byte(s)\n", sizeof (long long int));               
-	        printf("size of a float: %ld byte(s)\n", sizeof (float));                              
-						                                                                                             
-                return(0); 
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char c;
+	int d;
+	long int ld;
+	long long int lld;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(ld));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lld));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+	return (0);
 }
